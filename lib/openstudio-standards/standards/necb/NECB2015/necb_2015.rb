a65f4ed5de9dfee92bc28deb9ae8cc3ca88c4bc8
phylroy <phylroy.lopez@gmail.com>
2019-07-03 20:46:20 +0000

    refactored methods into domains.. still need to look at NECB2015

--- lib/openstudio-standards/standards/necb/NECB2015/necb_2015.rb
+++ lib/openstudio-standards/standards/necb/NECB2015/necb_2015.rb
@@ -67,8 +67,7 @@ class NECB2015 < NECB2011
     model = super(model: model,
                   epw_file: epw_file,
                   debug: debug,
-                  sizing_run_dir: sizing_run_dir
-)
+                  sizing_run_dir: sizing_run_dir)
     # NECB2015 Custom code
     # Do another sizing run to take into account adjustments to equipment efficiency etc. on capacities. This was done primarily
     # because the cooling tower loop capacity is affected by the chiller COP.  If the chiller COP is not properly set then
@@ -77,6 +76,7 @@ class NECB2015 < NECB2011
     if model_run_sizing_run(model, "#{sizing_run_dir}/SR2") == false
       raise("sizing run 2 failed!")
     end
+
     # Apply maxmimum loop pump power normalized by peak demand by served spaces as per NECB2015 5.2.6.3.(1)
     apply_maximum_loop_pump_power(model)
 
